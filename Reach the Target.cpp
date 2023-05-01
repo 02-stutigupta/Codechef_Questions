e28c9f4085028cda164acff3f0aa56d1d2ab85cd
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-01 14:19:10 +0530

    Create Reach the Target.cpp

---
+++ Reach the Target.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,a,b;
+	cin>>i;
+	while(i--)
+	{
+	    cin>>a>>b;
+	    cout<<a-b<<endl;
+	}
+	return 0;
+}
