e7e5f741108dc186aed5dc116ab4b9a104596004
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-01 14:13:49 +0530

    Create Chef Plays Ludo.cpp

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,p;
+	cin>>i;
+	while(i--)
+	{
+	    cin>>p;
+	    if(p==6) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
