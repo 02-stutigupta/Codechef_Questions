70d3eaa214554c8578b57efcb5c70fe501fc71af
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-01 14:10:51 +0530

    Create Water Consumption.cpp

---
+++ Water Consumption.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,w;
+	cin>>i;
+	while(i--)
+	{
+	    cin>>w;
+	    if(w>=2000) cout<<"Yes"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
