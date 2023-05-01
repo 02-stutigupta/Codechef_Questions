38d4029c0f7a88d2e84e7ef3344954423c0997d1
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-01 14:16:13 +0530

    Create Audible Range.cpp

---
+++ Audible Range.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,w;
+	cin>>i;
+	while(i--)
+	{
+	    cin>>w;
+	    if((w>=67)&&(w<=45000)) cout<<"Yes"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
