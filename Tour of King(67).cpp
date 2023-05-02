6e4856fc8082911fe56220550d6a7430ab61e623
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-02 17:56:46 +0530

    Create Tour of King(67).cpp

---
+++ Tour of King(67).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin>>n;
+	while(n--)
+	{
+	    cin>>a>>b;
+	    cout<<((a*5)+(b*7))<<endl;
+	}
+	return 0;
+}
