daefd42840474bdf13c12ff3ecfe7793daaa7c79
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-05 23:23:10 +0530

    Create Parity.cpp

---
+++ Parity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,i;
+	cin>>i;
+	while(i--)
+	{
+	    cin>>n;
+	    if(n%2==0) cout<<"Yes\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
