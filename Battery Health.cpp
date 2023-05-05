8f4cb3d78169df1e5f2675f1d067cd231bb80b6c
Stuti Gupta <77190516+02-stutigupta@users.noreply.github.com>
2023-05-05 23:26:18 +0530

    Create Battery Health.cpp

---
+++ Battery Health.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,n;
+	cin>>n;
+	while(n--)
+	{
+	    cin>>i;
+	    if(i>=80) cout<<"yes\n";
+	    else cout<<"no\n";
+	}
+	return 0;
+}
